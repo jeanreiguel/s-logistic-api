f5c3a90dfd244095f81996c0ff4b3242c8883625
jenasw <jeanrgl03@gmail.com>
2021-06-28 11:46:39 -0300

    ajeitar databases etc

--- src/main/java/br/com/senai/security/WebSecurityConfig.java
+++ src/main/java/br/com/senai/security/WebSecurityConfig.java
@@ -1,48 +1,67 @@
 package br.com.senai.security;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@AllArgsConstructor
-@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+@Configuration
+@AllArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private ImplementsUserDetailsService implementsUserDetailsService;
+    private JWTRequestFilter jwtRequestFilter;
 
     private static final String[] AUTH_LIST = {
             "/",
             "/pessoas",
-            "/pessoas/{pessoaId}"
+            "/pessoas/{pessoaId}",
+            "/roles",
+            "/roles/{nomeRole}",
+            "/role_usuarios/{roleUsuarioId}"
     };
 
     @Override
-    protected void configure(HttpSecurity http) throws  Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                //.antMatchers(HttpMethod.GET,"/entregas").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,AUTH_LIST).permitAll()
-                .antMatchers(HttpMethod.POST,AUTH_LIST).permitAll()
-                .antMatchers(HttpMethod.PUT,AUTH_LIST).permitAll()
-                .antMatchers(HttpMethod.DELETE,AUTH_LIST).permitAll();
- //                  .anyRequest().authenticated().and().formLogin().permitAll()
-  //              .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-    }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("jean").password("{noop}123456").roles("ADMIN");
-        auth.userDetailsService(implementsUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    }
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/bootstrap/**","/style/**");
+                .antMatchers(HttpMethod.GET, "/entregas").hasRole("ADMIN")
+                .antMatchers("/authenticate").permitAll()
+                .antMatchers(HttpMethod.GET, AUTH_LIST).permitAll()
+                .antMatchers(HttpMethod.POST, AUTH_LIST).permitAll()
+                .antMatchers(HttpMethod.PUT, AUTH_LIST).permitAll()
+                .antMatchers(HttpMethod.DELETE, AUTH_LIST).permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .deleteCookies("token").invalidateHttpSession(true);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //auth.inMemoryAuthentication().withUser("maria").password("{noop}123456").roles("ADMIN");
+        auth.userDetailsService(implementsUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/bootstrap/**", "/style/**");
+    }
 }
